e1ba53e6040b1ba2d6fc5ea4cf641abf35f9e9b4
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-08 12:12:20 +0100

    express intro done

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const express = require("express");
+const express = require("express")
 
-require("dotenv").config();
+require("dotenv").config() // require("dotenv/config")
 
 const app = express();
 
 // Build the server here
+
+app.use(express.static("public"))  //method to configure the application 
+//static directive and indicate the name of the "public" folder
+
+
+//this responds to all HTTP verbs
+
+app.all("/", (request,response)=>{
+    response.sendFile(__dirname+ "/views/index.html")
+})
+
+
+app.all("/home", (request,response)=>{
+    response.sendFile(__dirname+ "/views/home.html")
+})
+
+
+
+app.listen(3000,()=>console.log("App listening on port 3000"))
---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+console.log("Hi I'm a static file running on the client!")
